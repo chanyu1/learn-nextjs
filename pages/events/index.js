2598261766821ff8af15147fab0b95e6381b1c0e
chanyu1 <haneul1ij@gmail.com>
2022-11-19 19:07:20 +0900

    feat: events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,24 @@
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+
+import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
+import EventSearch from "../../components/events/event-search";
+
 export default function AllEventsPage() {
+  const router = useRouter();
+  const events = getAllEvents();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  }
+
   return (
-    <div>
-      <h1>All Events</h1>
-    </div>
+    <Fragment>
+      <EventSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </Fragment>
   );
 }
